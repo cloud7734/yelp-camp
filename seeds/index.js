3ed2ced2a48722e630a6a6edd4460cf4680344a4
cloud7734 <Jake.Nugent413@gmail.com>
2023-06-20 18:06:06 -0500

    Add basic bootstrap Add seed images

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/9046579',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia aliquam reiciendis magni, eos ipsa obcaecati commodi libero id atque saepe, facere suscipit dolor! Unde fugit ea vero. Neque, commodi.',
+            price
         });
         await camp.save();
     }
