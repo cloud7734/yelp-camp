e3a40ca878dd0bd9d873699350e0d4046a2e4efe
cloud7734 <Jake.Nugent413@gmail.com>
2023-06-24 11:24:07 -0500

    Add campground and review user authorizations

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '64970a8baef2781c452e1799',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/9046579',
