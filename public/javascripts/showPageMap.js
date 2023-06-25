b88b5e8f7098cb4108deffc614f19e4762755adc
cloud7734 <Jake.Nugent413@gmail.com>
2023-06-25 10:39:09 -0500

    Add cluster map and pins

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/outdoors-v12', 
+  style: 'mapbox://styles/mapbox/light-v11', 
   center: campground.geometry.coordinates, 
   zoom: 10
 });
--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
+    for(let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -42,7 +42,10 @@ const seedDB = async() => {
               ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia aliquam reiciendis magni, eos ipsa obcaecati commodi libero id atque saepe, facere suscipit dolor! Unde fugit ea vero. Neque, commodi.',
             price,
-            geometry: { type: 'Point', coordinates: [ -113.133115, 47.020078 ] }
+            geometry: { type: 'Point', coordinates: [ 
+              cities[random1000].longitude,
+              cities[random1000].latitude
+            ] }
         });
         await camp.save();
     }
